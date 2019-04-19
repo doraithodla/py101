3f6265090acf98da7ea5283d21d329d0bfa633be
Kunkuma Mithun Balaji Venkatesan <mithun14leo@gmail.com>
2019-04-19 15:39:24 +0530

    Delete dbops.py

--- database/dbops.py
+++
@@ -1,9 +0,0 @@
-# create qa database
-# create a qa
-import sqlite3
-conn = sqlite3.connect('qa.db')
-cur = conn.cursor()
-cur.execute('drop table if exists qa')
-cur.execute('create table qa (question text, answer text)')
-conn.close()
-
