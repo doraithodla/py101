d8723c6454ccc6c4ba4bee0700ba496ebfaa3600
Dorai Thodla <doraithodla@users.noreply.github.com>
2021-02-02 22:26:37 +0530

    bug fix

--- learnpy/sum_of_n.py
+++ learnpy/sum_of_n.py
@@ -1,6 +1,7 @@
 n = int(input("Enter the value:"))
 sum = 0
 while (n != 0):
-    sum += int(input())
+    print(n)
+    sum += n
     n -= 1
 print("The sum is: ", sum)
--- learnpy/sum_of_range.py
+++ learnpy/sum_of_range.py
@@ -2,5 +2,6 @@ a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of a: "))
 sum = 0
 for i in range(a, (b + 1)):
+    print(i)
     sum += i
 print(sum)
