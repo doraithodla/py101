d549e5febf1fa136b3457dee924ee06a5e80b52e
Dorai Thodla <doraithodla@users.noreply.github.com>
2022-09-03 06:42:19 +0530

    Update random_ints.py

--- learnpy/random_ints.py
+++ learnpy/random_ints.py
@@ -11,3 +11,10 @@ list1 = []
 for i in range(10):
     list1.append(randint(0, 100))
 print(list1)
+
+'''
+Practice Exercises:
+1. Can you change the program to create 10 floating point numbers?
+2. Can you explore the random module and list three functions that you may want to use?
+3. Can you generate random numbers but store only odd numbers in the list?
+
