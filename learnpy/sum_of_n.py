08d035f7d8be78e1cefd3801e98b76c52f3a4786
Dorai Thodla <doraithodla@users.noreply.github.com>
2022-09-03 08:01:22 +0530

    Updated description and added practice problems

--- learnpy/sum_of_n.py
+++ learnpy/sum_of_n.py
@@ -1,3 +1,12 @@
+'''
+sum_of_n.py - Calculate the sum of n natural numbers
+
+Concepts covered:
+- Numeric input
+- while statement
+- Addition of numbers
+'''
+
 n = int(input("Enter the value:"))
 sum = 0
 while (n != 0):
@@ -5,3 +14,10 @@ while (n != 0):
     sum += n
     n -= 1
 print("The sum is: ", sum)
+
+'''Practice problems:
+1. What are some alternate methods of finding the sum of n natural numbers?
+2. Rewrite the program using for loop instead of while
+3. Rewrite the program using incrementing the number rather than decrementing it
+'''
+
